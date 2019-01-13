089018721524387867aadbf61d9744694a98b221
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-13 18:38:15 +0900

    provide database

---
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/AppDatabase.kt
@@ -0,0 +1,8 @@
+package jp.cordea.roominmemoryarchitecturedemo
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+}
---
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/AppModule.kt
@@ -0,0 +1,17 @@
+package jp.cordea.roominmemoryarchitecturedemo
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+
+@Module
+class AppModule {
+    @Provides
+    fun provideContext(application: Application): Context = application.applicationContext
+
+    @Provides
+    fun provideAppDatabase(context: Context): AppDatabase =
+        Room.databaseBuilder(context, AppDatabase::class.java, "roominmemoryarchitecturedemo").build()
+}
