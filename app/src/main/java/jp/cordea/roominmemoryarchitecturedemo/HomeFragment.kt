2e46f4cc9046a3c811469e2735a557cac58bb968
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-13 19:18:02 +0900

    add new fragment

---
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/HomeFragment.kt
@@ -0,0 +1,24 @@
+package jp.cordea.roominmemoryarchitecturedemo
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_home, container, false)
+}
