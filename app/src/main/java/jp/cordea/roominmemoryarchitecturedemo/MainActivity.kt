9819665dd0abcd71c6ce5caa4e0ca356692c5044
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-13 21:50:54 +0900

    display fragments

--- app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainActivity.kt
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainActivity.kt
@@ -1,37 +1,33 @@
 package jp.cordea.roominmemoryarchitecturedemo
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+        val fragment = when (item.itemId) {
+            R.id.navigation_home -> HomeFragment.newInstance()
+            R.id.navigation_dashboard -> DashboardFragment.newInstance()
+            R.id.navigation_notifications -> NotificationFragment.newInstance()
+            else -> throw IllegalStateException()
         }
-        false
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, HomeFragment.newInstance())
+            .commit()
     }
 }
