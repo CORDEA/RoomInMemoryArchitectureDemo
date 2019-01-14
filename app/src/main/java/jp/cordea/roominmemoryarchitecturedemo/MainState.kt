62612b6f1d3fdf4ca230c66084829659b70f4c82
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-14 12:16:35 +0900

    store count

--- app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainState.kt
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainState.kt
@@ -3,7 +3,10 @@ package jp.cordea.roominmemoryarchitecturedemo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    tableName = "main_state"
+)
 class MainState(
-    @PrimaryKey val id: Int
+    @PrimaryKey val id: Long,
+    val count: Int
 )
