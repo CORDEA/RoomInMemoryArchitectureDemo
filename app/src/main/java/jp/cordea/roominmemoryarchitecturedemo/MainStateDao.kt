334aad61c6589685c87887d88f527ca3ab7291d4
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-14 12:16:44 +0900

    get state

--- app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainStateDao.kt
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainStateDao.kt
@@ -1,11 +1,16 @@
 package jp.cordea.roominmemoryarchitecturedemo
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface MainStateDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertMainState(mainState: MainState)
+
+    @Query("SELECT * FROM main_state WHERE id = :id")
+    fun getMainState(id: Long): LiveData<MainState>
 }
