5a30a8758ddd55fbc96607bdc25867e21f771012
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-14 14:39:06 +0900

    observe

--- app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/dashboard/DashboardFragment.kt
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/dashboard/DashboardFragment.kt
@@ -1,20 +1,33 @@
 package jp.cordea.roominmemoryarchitecturedemo.dashboard
 
-
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.jakewharton.rxbinding3.view.clicks
+import dagger.android.support.AndroidSupportInjection
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
 import jp.cordea.roominmemoryarchitecturedemo.databinding.FragmentDashboardBinding
+import javax.inject.Inject
 
 class DashboardFragment : Fragment() {
     companion object {
         fun newInstance() = DashboardFragment()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @Inject
+    lateinit var viewModel: Lazy<DashboardViewModel>
+
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
     }
 
     override fun onCreateView(
@@ -22,6 +35,19 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentDashboardBinding.inflate(inflater, container, false)
+
+        binding.button.clicks()
+            .subscribeBy { viewModel.value.clicked() }
+            .addTo(compositeDisposable)
+
+        viewModel.value.text
+            .observe(this, Observer { binding.state.text = it })
+
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 }
