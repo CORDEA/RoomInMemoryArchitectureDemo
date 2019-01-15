f0eac22909d8c56242b793e61d98f0dbf9f697c2
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-15 21:17:16 +0900

    enable databinding

--- app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/notification/NotificationFragment.kt
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/notification/NotificationFragment.kt
@@ -5,21 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import jp.cordea.roominmemoryarchitecturedemo.R
+import jp.cordea.roominmemoryarchitecturedemo.databinding.FragmentNotificationBinding
 
 class NotificationFragment : Fragment() {
     companion object {
         fun newInstance() = NotificationFragment()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_notification, container, false)
-
+    ): View {
+        val binding = FragmentNotificationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
