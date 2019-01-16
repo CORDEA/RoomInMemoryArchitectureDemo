4ab726ebbc9acf90b07ec2f149c6f6e056092539
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-16 22:12:04 +0900

    observe

--- app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainActivity.kt
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jakewharton.rxbinding3.view.clicks
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import jp.cordea.roominmemoryarchitecturedemo.dashboard.DashboardFragment
 import jp.cordea.roominmemoryarchitecturedemo.databinding.ActivityMainBinding
 import jp.cordea.roominmemoryarchitecturedemo.home.HomeFragment
@@ -18,6 +22,11 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+
+    @Inject
+    lateinit var viewModel: Lazy<MainViewModel>
+
+    private val compositeDisposable = CompositeDisposable()
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         val fragment = when (item.itemId) {
@@ -39,10 +48,21 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         binding.navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
+        binding.fab.clicks()
+            .concatMapCompletable { viewModel.value.clicked() }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+            .addTo(compositeDisposable)
+
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, HomeFragment.newInstance())
             .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> = dispatchingAndroidInjector
--- app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainActivityModule.kt
+++ app/src/main/java/jp/cordea/roominmemoryarchitecturedemo/MainActivityModule.kt
@@ -1,6 +1,10 @@
 package jp.cordea.roominmemoryarchitecturedemo
 
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 import jp.cordea.roominmemoryarchitecturedemo.dashboard.DashboardFragmentModule
 import jp.cordea.roominmemoryarchitecturedemo.home.HomeFragmentModule
@@ -14,6 +18,26 @@ import jp.cordea.roominmemoryarchitecturedemo.notification.NotificationFragmentM
     ]
 )
 interface MainActivityModule {
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(
+        modules = [
+            MainActivityBindModule::class
+        ]
+    )
     fun contributeMainActivity(): MainActivity
+}
+
+@Module
+abstract class MainActivityBindModule {
+    @Module
+    companion object {
+        @JvmStatic
+        @Provides
+        fun provideViewModel(
+            activity: ComponentActivity,
+            factory: ViewModelFactory<MainViewModel>
+        ): Lazy<MainViewModel> = activity.viewModels { factory }
+    }
+
+    @Binds
+    abstract fun bindComponentActivity(activity: MainActivity): ComponentActivity
 }
